1f0a877c4ec3bc67b243af05cdedb520dd2e8601
JinJaeJee <jpluemphiromnat@gmail.com>
2023-08-28 17:17:49 +0700

    feat: create all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import { CustomerReviews, Footer, Hero, Nav, PopularProducts, Services, Specialoffer, Subscribe, Superquality } from "./sections";
+
+const App = () => (
+  <main className="relative">
+    <Nav/>
+    <section className="xl:padding-1 wide:padding-r pdding-b">
+      <Hero/>
+    </section>
+    <section className="padding">
+    <PopularProducts/>
+    </section>
+    <section className="padding">
+    <Superquality/>
+    </section>
+    <section className="padding">
+    <Services/>
+    </section>
+    <section className="padding">
+    <Specialoffer/>
+    </section>
+    <section className="bg-pale-blue padding" >
+    <CustomerReviews/>
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+    <Subscribe/>
+    </section>
+    <section className="padding-x py-10">
+    <Footer/>
+    </section>
+  </main>
+);
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <h1 className="text-3xl font-bold underline">hello world</h1>
+//   )
+// }
---
+++ src/sections/index.js
@@ -0,0 +1,14 @@
+import CustomerReviews from "./CustomerReviews"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Nav from "./Nav"
+import PopularProducts from "./PopularProducts"
+import Services from "./Services"
+import Specialoffer from "./SpecialOffer"
+import Subscribe from "./Subscribe"
+import Superquality from "./Superquality"
+
+export {
+    CustomerReviews, Footer, Hero, Nav, PopularProducts, Services, Specialoffer,
+    Subscribe, Superquality
+}
