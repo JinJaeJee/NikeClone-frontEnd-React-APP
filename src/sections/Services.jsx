665530d67b860be7532c01bc35869b0b55fcd6c7
JinJaeJee <jpluemphiromnat@gmail.com>
2023-08-31 19:50:41 +0700

    feat: add sevice section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,15 @@
+import ServiceCard from "../components/ServiceCard"
+import { services } from "../constants"
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center flex-warp gap-9" >
+      {services.map((service,index)=> (
+        <ServiceCard
+          key={index} {...service}
+        />
+      ))}
+    </section>
   )
 }
 
